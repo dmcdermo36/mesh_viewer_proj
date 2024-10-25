92e04cc41f3b0683786b877ec1dcff4438106a84
dmcdermott <dmcdermo36@gmail.com>
2024-10-24 23:36:25 -0700

    Update Python versions in Dockerfiles

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,46 @@
 import streamlit as st
+import pyvista as pv
+# from pyvista.examples import planefile
+from stpyvista import stpyvista
 
-st.title('Interactive Mesh Dashboard')
-st.write('This is where the 3D mesh visualization will go.')
+from stpyvista.utils import start_xvfb
+
+if "IS_XVFB_RUNNING" not in st.session_state:
+  start_xvfb()
+  st.session_state.IS_XVFB_RUNNING = True 
+
+
+st.title("Interactive 3D Mesh Visualization with stpyvista")
+st.write("This is a demo of the `stpyvista` Streamlit component.")
+
+# Set up a PyVista plotter
+plotter = pv.Plotter(window_size=(500, 900))
+plotter.view_isometric()
+plotter.set_background("darkgray")
+# plotter.camera.zoom('tight')
+
+# mesh = pv.Sphere()
+mesh = pv.Cylinder()
+# mesh = pv.read(planefile)
+actor = plotter.add_mesh(mesh, color="lightblue", show_edges=True)
+
+
+
+color = st.sidebar.color_picker("pick a color", "#82E0AA")
+# camera_position = plotter.camera_position
+# print(camera_position)
+
+actor.prop.color = pv.Color(color)
+
+# plotter.camera_position = camera_position
+# stpyvista(plotter, key='pv_shape') # this key is troublesome!
+
+stpyvista(plotter)
+
+st.write("More text after plotters")
+
+
+
+print('made it to the end')
+# uvicorn main:app --reload # backend
+# streamlit run app.py # frontend
---
+++ frontend/app_troubleshoot.py
@@ -0,0 +1,25 @@
+import pyvista as pv
+import streamlit as st
+from stpyvista import stpyvista
+
+st.title("A cube")
+st.info("""Code adapted from https://docs.pyvista.org/user-guide/jupyter/pythreejs.html#scalars-support""")
+
+## Initialize a plotter object
+plotter = pv.Plotter(window_size=[400,400])
+
+## Create a mesh with a cube 
+mesh = pv.Cube(center=(0,0,0))
+
+## Add some scalar field associated to the mesh
+mesh['myscalar'] = mesh.points[:, 2] * mesh.points[:, 0]
+
+## Add mesh to the plotter
+plotter.add_mesh(mesh, scalars='myscalar', cmap='bwr')
+
+## Final touches
+plotter.view_isometric()
+plotter.background_color = 'white'
+
+## Send to streamlit
+stpyvista(plotter, key="pv_cube")
